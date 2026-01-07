6e8ccefa0ccc75806a465a215fbb05f67e8246f9
touspourtech <touspourtech@gmail.com>
2026-01-07 11:29:14 +0100

    Premier commit : Création

---
+++ src/assets/javascript/form.js
@@ -0,0 +1,62 @@
+import { GoogleGenAI } from "@google/genai";    
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+  
+const ai = new GoogleGenAI({apiKey: "AIzaSyD9vhM9iHGvz2jee_zvZ2lDeQjaDYZ0hJM"});
+
+export class ContentForm {
+    #searchInput;
+    #contentInput;
+    #form;
+
+    constructor() {
+	    // mapping the class attributes to the HTML form input
+        this.#form = document.querySelector("#content-form");
+        this.#searchInput = document.querySelector("#seo-search");
+        this.#contentInput = document.querySelector("#content");
+
+		// listening to the form submit event
+        this.#form.addEventListener("submit", (event) => this.#submit(event, this));
+    }
+
+    async #request(terms){
+        const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: terms,
+        });
+		
+		// hiding the loader modal whn receiving the response
+        let modal = document.querySelector("div.modal");
+        modal.style.display = "none";
+
+		// preparing the section for inserting the response
+        let section = document.querySelector("#gemini-answer");
+        section.scrollIntoView();
+        
+        // load the markdown turned html in the section
+        section.innerHTML = marked.parse(response.text);
+    }
+/* JavaScript often loses the this reference when you use an event inside the class, which is why we use the that parameter */
+    #submit(event, that)
+    {
+	    // prevent the form from reloading the page
+        event.preventDefault();
+        
+        // display the loader modal
+        let modal = document.querySelector("div.modal");
+        modal.style.display = "grid";
+        
+        // get the key search value
+        let search = that.#searchInput.value;
+        
+        // get the content value
+        let content = that.#contentInput.value;
+
+		// build the gemini request from the inputs
+        let terms = `Bonjour, pourrais-tu me calculer un score SEO pour un contenu que je vais 
+        t'envoyer en format Markdown. Les termes de recherches pour lesquels calculer sont les suivants : ${search}.
+        Le contenu est le suivant : ${content}. J'aurais besoin d'une réponse synthétique au format Markdown avec un score sur 20. Merci !`;
+
+        // call our async method
+        that.#request(terms);
+    }
+}
